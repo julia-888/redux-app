dc0ac2510c5524f9eacb57d8b43e8e01ef8f0d85
julia-888 <71026631+julia-888@users.noreply.github.com>
2024-07-12 12:34:41 +0300

    add styles in Table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
+
+
 
 const Table = () => {
+  const getApiData = async () => {
+    const response = await axios(
+      "https://jsonplaceholder.typicode.com/todos/"
+    )
+  
+    // Обновим состояние
+    setUsers(response);
+  };
+
+  
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getApiData()
+  }, [])
+
+  console.log(users)
   return (
+    <>
     <TableWrap>
-      <tr>
-        <th>Первая</th>
-        <th>Первая</th>
-        <th>Первая</th>
-        <th>Первая</th>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>4</td>
-        <td>4</td>
-        <td>4</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>{users[0].title}</th>
+          <th>Первая</th>
+          <th>Первая</th>
+          <th>Первая</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+        </tr>
+      </tbody>
     </TableWrap>
+    </>
   )
 }
 
 export default Table;
 
 const TableWrap = styled.table`
-  
+  border: solid;
+  border-collapse: collapse;
+
+  th {
+    border: solid;
+  }
+
+  td {
+    border: solid;
+  }
 `
