e2f272fb16ee342df23ca1d7acc9c3fdd0b6c8dd
julia_888 <adamidijulia@gmail.com>
2024-11-05 18:25:45 +0300

    init commit: install tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import FirstPage from "./components/FirstPage";
+// import SecondPage from "./components/SecondPage";
+// import { useSelector } from "react-redux";
+import "./input.tailwind.css";
+
+function App() {
+  // const openedPage = useSelector((state) => state.openedPage.value);
+
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline bg-cyan-400">Hello World!</h1>
+      {/* {openedPage == 1 ? <FirstPage /> : <SecondPage />} */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+import React from "react";
+// import { Provider } from "react-redux";
+// import { store } from "./state/store";
+
+createRoot(document.getElementById("root")).render(
+  // <Provider store={store}>
+  <App />
+  // </Provider>
+);
